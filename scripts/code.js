d140bcc62936245e560b6ed33754495dbb7895e9
Meg <meghan_greco@yahoo.com>
2023-11-04 19:29:19 -0400

    second

--- scripts/code.js
+++ scripts/code.js
@@ -15,3 +15,16 @@ Plotly.newPlot(
   }
 );
 });
+
+//Credit to W3 https://www.w3schools.com/howto/howto_js_typewriter.asp
+var i = 0;
+var txt = 'Lorem ipsum typing effect!'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
